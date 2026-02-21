2bca40362f131f77c248944e0b749ea298f17662
Rogério Marques <rjsmarques.ti@gmail.com>
2026-02-21 19:16:03 -0300

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -8,7 +8,7 @@ declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-// Exporta com nome (permite import { prisma } from ...)
+// O "export const" é obrigatório para funcionar o import { prisma }
 export const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
